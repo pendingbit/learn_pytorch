5eb2640abc1fa0268425c5bc7c9f66dbb9cd23fe
joey <pending_bit@outlook.com>
2024-07-22 17:15:09 -0700

    new 03_PyTorch_ComputerVision.py

---
+++ 03_PyTorch_ComputerVision.py
@@ -0,0 +1,15 @@
+# Import PyTorch
+import torch
+from torch import nn
+
+# Import torchvision
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# Import matplotlib for visualization
+import matplotlib.pyplot as plt
+
+# Check versions
+print(f"PyTorch version: {torch.__version__}\ntorchvision version: {torchvision.__version__}")
+
