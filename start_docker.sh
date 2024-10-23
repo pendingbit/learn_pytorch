92f2f7f52502e080705141bc16cdcb11d3b18720
joey <pending_bit@outlook.com>
2024-10-23 16:20:00 +0800

    modify floder strcut

--- start_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-#sudo docker run -v /home/joey/:/home/joey/ -it --rm --runtime nvidia --network host my_updated_docker:v04 /home/joey/github/learn_pytorch/start_jupyter.sh
-sudo docker run -v /home/joey/:/home/joey/ -it --rm --runtime nvidia --network host my_updated_docker:v04 
-
-
